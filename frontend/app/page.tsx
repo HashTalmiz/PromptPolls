c3d84ef2602360bf2a786a6afbe88987455b8ca9
Talmiz Ahmed <talmiz.tech@gmail.com>
2023-09-16 23:24:15 +0530

    Scroll to my polls

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ChevronRightIcon } from '@heroicons/react/20/solid';
 import Link from 'next/link';
-import { data } from 'autoprefixer';
 
 interface PollType {
   id: string;
@@ -48,18 +47,20 @@ export default async function LandingPage() {
           <div className="manageTabs text-center px-[40px] py-10 max-w-sm">
             <h4 className="text-xl font-bold mb-5">Your Past Polls</h4>
             <hr style={{ color: 'darkgrey', width: '50%', margin: 'auto' }} />
-            {userData.length > 0 ? (
-              userData.map((poll, i) => (
-                <Link key={i} href={`/poll/${poll.id}`}>
-                  <div className="border-2 my-5 flex w-full justify-between rounded-lg px-4 py-2 text-left text- font-medium">
-                    <p>{poll.title}</p>
-                    <ChevronRightIcon className="h-7 w-7" />
-                  </div>
-                </Link>
-              ))
-            ) : (
-              <div className="mt-5">{emptyMsg}</div>
-            )}
+            <div className="max-h-[200px] max-w-[400px]" style={{overflowY: "auto"}}>
+              {userData.length > 0 ? (
+                userData.map((poll, i) => (
+                  <Link key={i} href={`/poll/${poll.id}`}>
+                    <div className="border-2 my-5 flex w-full justify-between rounded-lg px-4 py-2 text-left text- font-medium">
+                      <p>{poll.title}</p>
+                      <ChevronRightIcon className="h-7 w-7" />
+                    </div>
+                  </Link>
+                ))
+              ) : (
+                <div className="mt-5">{emptyMsg}</div>
+              )}
+            </div>
           </div>
         </div>
       </section>
