58053ee4d17b3f30cbbd1f49dd6492f94d401be1
Talmiz Ahmed <talmiz.tech@gmail.com>
2023-10-03 23:21:19 +0530

    docker compose additions, still blocked with prisma and postgress

--- backend/src/db/redis.ts
+++ backend/src/db/redis.ts
@@ -6,7 +6,9 @@ import { EntityId } from 'redis-om'
 
 /* pulls the Redis URL from .env */
 const url = process.env.REDIS_URL
-const redis = createClient()
+const redis = createClient({
+    url: 'redis://redis-om:6379'
+})
 async function init() {
     redis.on('error', (err) => console.log('Redis Client Error', err));
     await redis.connect();
